600471d5cf5394b04f3ee952f0c1d1d8a6ea946f
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-08-24 11:52:27 +0200

    Create Achievement Repository Implementation

---
+++ src/main/java/com/buzar/praxibackend/repository/AchievementRepositoryImpl.java
@@ -0,0 +1,101 @@
+package com.buzar.praxibackend.repository;
+
+import com.buzar.praxibackend.entity.Achievement;
+import com.buzar.praxibackend.entity.User;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@Transactional
+public class AchievementRepositoryImpl implements AchievementRepository {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public List<Achievement> findAll() {
+
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query query = currentSession.createQuery("from Achievement", Achievement.class);
+        List<Achievement> achievementList = query.getResultList();
+
+        return achievementList;
+    }
+
+    @Override
+    public Achievement findById(int achievementId) {
+
+        Session currentSession = entityManager.unwrap(Session.class);
+        Achievement tempAchievement = currentSession.get(Achievement.class, achievementId);
+
+        return tempAchievement;
+    }
+
+
+    @Override
+    public void saveOrUpdate(Achievement tempAchievement) {
+
+        Session currentSession = entityManager.unwrap(Session.class);
+        currentSession.saveOrUpdate(tempAchievement);
+    }
+
+    @Override
+    public void addAchievementToUser(int achievementId, int userId) {
+
+        Session currentSession = entityManager.unwrap(Session.class);
+        User tempUser = currentSession.get(User.class, userId);
+        Achievement tempAchievement = currentSession.get(Achievement.class, achievementId);
+        tempUser.addOpenAch(tempAchievement);
+        currentSession.saveOrUpdate(tempUser);
+        currentSession.saveOrUpdate(tempAchievement);
+    }
+
+    @Override
+    public void removeAchievementFromUser(int achievementId, int userId) {
+
+        Session currentSession = entityManager.unwrap(Session.class);
+        User tempUser = currentSession.get(User.class, userId);
+        Achievement tempAchievement = currentSession.get(Achievement.class, achievementId);
+        tempUser.removeOpenAch(tempAchievement);
+        currentSession.saveOrUpdate(tempUser);
+        currentSession.saveOrUpdate(tempAchievement);
+    }
+
+    @Override
+    public void finishAchievementForUser(int achievementId, int userId) {
+
+        Session currentSession = entityManager.unwrap(Session.class);
+        User tempUser = currentSession.get(User.class, userId);
+        Achievement tempAchievement = currentSession.get(Achievement.class, achievementId);
+        tempUser.addFinishedAch(tempAchievement);
+        tempUser.removeOpenAch(tempAchievement);
+        currentSession.saveOrUpdate(tempUser);
+        currentSession.saveOrUpdate(tempAchievement);
+    }
+
+    @Override
+    public void takeAchievementFromUser(int achievementId, int userId) {
+
+        Session currentSession = entityManager.unwrap(Session.class);
+        User tempUser = currentSession.get(User.class, userId);
+        Achievement tempAchievement = currentSession.get(Achievement.class, achievementId);
+        tempUser.removeFinishedAch(tempAchievement);
+        currentSession.saveOrUpdate(tempUser);
+        currentSession.saveOrUpdate(tempAchievement);
+    }
+
+    @Override
+    public void delete(int achievementId) {
+
+        Session currentSession = entityManager.unwrap(Session.class);
+        Achievement tempAchievement = currentSession.get(Achievement.class, achievementId);
+        currentSession.delete(tempAchievement);
+    }
+}
+
