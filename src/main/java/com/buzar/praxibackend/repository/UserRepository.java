685b8c3a81852109355fb07e3592558f0df6c41b
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-08-24 11:52:05 +0200

    Update UserRepository.java

--- src/main/java/com/buzar/praxibackend/repository/UserRepository.java
+++ src/main/java/com/buzar/praxibackend/repository/UserRepository.java
@@ -3,12 +3,15 @@ package com.buzar.praxibackend.repository;
 import com.buzar.praxibackend.entity.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository{
 
     List<User> findAll();
 
     User findByUserId(int userId);
+
+    Optional<User> findByUsername(String tempUsername);
 
     void saveOrUpdate(User user);
 
