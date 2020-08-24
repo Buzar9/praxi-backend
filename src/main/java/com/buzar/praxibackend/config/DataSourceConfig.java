3e36c8f29c783ec070d3e538b82aec4aa26d0cfa
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-08-24 09:19:04 +0200

    Create DataSourceConfig.java
    Database connection configuration

---
+++ src/main/java/com/buzar/praxibackend/config/DataSourceConfig.java
@@ -0,0 +1,34 @@
+package com.buzar.praxibackend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan("com.buzar.praxibackend")
+@PropertySource(value = "application.properties")
+public class DataSourceConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource myDataSource() throws Exception {
+
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+        dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+        dataSource.setPassword(environment.getProperty("spring.datasource.password"));
+
+        return dataSource;
+    }
+}
