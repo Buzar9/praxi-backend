cfce78b25c290a5884c02e18f5bf1a85132dc12e
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-08-24 08:17:35 +0200

    Initial commit

---
+++ src/test/java/com/buzar/praxibackend/PraxiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buzar.praxibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraxiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
