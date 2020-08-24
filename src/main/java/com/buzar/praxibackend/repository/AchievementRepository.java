ae49b2b6669eb319e270a8a6dc48cf2a6010d8ac
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-08-24 09:52:06 +0200

    Create Achievement Repository Interface

---
+++ src/main/java/com/buzar/praxibackend/repository/AchievementRepository.java
@@ -0,0 +1,24 @@
+package com.buzar.praxibackend.repository;
+
+import com.buzar.praxibackend.entity.Achievement;
+
+import java.util.List;
+
+public interface AchievementRepository {
+
+    List<Achievement> findAll();
+
+    Achievement findById(int achievementId);
+
+    void saveOrUpdate(Achievement tempAchievement);
+
+    void addAchievementToUser(int achievementId, int userId);
+
+    void removeAchievementFromUser(int achievementId, int userId);
+
+    void finishAchievementForUser(int achievementId, int userId);
+
+    void takeAchievementFromUser(int achievementId, int userId);
+
+    void delete(int achievementId);
+}
