db57e22f0a428c8931e29aedf8ea0752ffae5db4
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-08-24 12:31:12 +0200

    Update PraxiBackendApplication.java
    Add @EnableConfigurationProperties to handle FileStorageProperties

--- src/main/java/com/buzar/praxibackend/PraxiBackendApplication.java
+++ src/main/java/com/buzar/praxibackend/PraxiBackendApplication.java
@@ -1,9 +1,12 @@
 package com.buzar.praxibackend;
 
+import com.buzar.praxibackend.properties.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({FileStorageProperties.class})
 public class PraxiBackendApplication {
 
 	public static void main(String[] args) {
