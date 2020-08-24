34a9cc9976d469dcf78fe3640ef2924ada8070ea
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-08-24 11:51:58 +0200

    Create Quest Repository interface

---
+++ src/main/java/com/buzar/praxibackend/repository/QuestRepository.java
@@ -0,0 +1,16 @@
+package com.buzar.praxibackend.repository;
+
+import com.buzar.praxibackend.entity.Quest;
+
+import java.util.List;
+
+public interface QuestRepository {
+
+    List<Quest> findAll();
+
+    Quest findById(int questId);
+
+    void saveOrUpdate(Quest tempQuest, int achievementId);
+
+    void delete(int questId);
+}
