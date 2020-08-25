9209b2c55c1c7d4656ffd66c85449a3b4407a784
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-08-25 09:23:03 +0200

    Create QuestController.java

---
+++ src/main/java/com/buzar/praxibackend/controller/QuestController.java
@@ -0,0 +1,52 @@
+package com.buzar.praxibackend.controller;
+
+import com.buzar.praxibackend.entity.Quest;
+import com.buzar.praxibackend.repository.QuestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/quests")
+@CrossOrigin(origins = "*")
+public class QuestController {
+
+    @Autowired
+    private QuestRepository questRepositoryImpl;
+
+    @GetMapping
+    public List<Quest> questList(){
+
+        return questRepositoryImpl.findAll();
+    }
+
+    @GetMapping("{questId}")
+    public Quest findById(@PathVariable int questId) {
+
+        return questRepositoryImpl.findById(questId);
+    }
+
+    @PostMapping("/achievement/{achievementId}")
+    public void saveQuest(@PathVariable int achievementId,
+                          @RequestBody Quest tempQuest){
+
+        tempQuest.setQuestId(0);
+        questRepositoryImpl.saveOrUpdate(tempQuest, achievementId);
+    }
+
+    @PutMapping("{questId}/achievement/{achievementId}")
+    public void updateQuest(@PathVariable int questId,
+                            @PathVariable int achievementId,
+                            @RequestBody Quest tempQuest) {
+
+        tempQuest.setQuestId(questId);
+        questRepositoryImpl.saveOrUpdate(tempQuest, achievementId);
+    }
+
+    @DeleteMapping("{questId}")
+    public void deleteQuest(@PathVariable int questId) {
+
+        questRepositoryImpl.delete(questId);
+    }
+}
