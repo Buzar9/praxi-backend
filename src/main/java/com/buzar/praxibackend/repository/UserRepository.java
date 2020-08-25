dfd2089511982d52714d94aa10c57bf476cb3d62
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-08-25 09:34:37 +0200

    Refactor

--- src/main/java/com/buzar/praxibackend/repository/UserRepository.java
+++ src/main/java/com/buzar/praxibackend/repository/UserRepository.java
@@ -3,14 +3,13 @@ package com.buzar.praxibackend.repository;
 import com.buzar.praxibackend.entity.User;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserRepository{
 
     List<User> findAll();
 
     User findByUserId(int userId);
-    
+
     void saveOrUpdate(User user);
 
     void deleteUser(int userId);
