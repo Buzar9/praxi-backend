657b7452cb2ab53a833912a9c3e138b90b189d18
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-08-25 09:23:12 +0200

    Create UserController.java

---
+++ src/main/java/com/buzar/praxibackend/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.buzar.praxibackend.controller;
+
+import com.buzar.praxibackend.entity.User;
+import com.buzar.praxibackend.repository.UserRepository;
+import com.buzar.praxibackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin(origins = "*")
+public class UserController {
+
+    @Autowired
+    private UserService userServiceImpl;
+
+    @Autowired
+    private UserRepository userRepositoryImpl;
+
+    @GetMapping
+    public List<User> findAll() {
+
+        return userRepositoryImpl.findAll();
+    }
+
+    @GetMapping("{userId}")
+    public User findUserById (@PathVariable(value = "userId") int userId) {
+
+        User tempUser = userRepositoryImpl.findByUserId(userId);
+
+        return tempUser;
+    }
+
+    @PostMapping
+    public void addUser(@RequestBody User tempUser) {
+
+        userServiceImpl.addUser(tempUser);
+    }
+
+    @PutMapping("{userId}")
+    public void updateUser(@PathVariable int userId,
+                           @RequestBody User tempUser) {
+
+        userServiceImpl.updateUser(userId, tempUser);
+    }
+
+    @DeleteMapping("{userId}")
+    public String deleteUser(@PathVariable(value = "userId") int userId) {
+
+        userRepositoryImpl.deleteUser(userId);
+
+        return "Deleted user id - " + userId;
+    }
+}
